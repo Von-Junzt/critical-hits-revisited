4cf707de1957e20668bc103e16bfc81f7eb1e809
aushilfsalien <nils.prior@outlook.de>
2024-04-04 13:13:08 +0200

    further comments to improve readability

--- macros/criticalHitRollEffects.js
+++ macros/criticalHitRollEffects.js
@@ -1,3 +1,4 @@
+// this macro is used to roll for critical hits and fumbles. it is added to the critical hit item in foundry vtt
 if (args[0].macroPass == 'postActiveEffects' && workflow.isCritical) {
     rollForCriticalHits(workflow);
 } else if (args[0].macroPass == 'postActiveEffects' && workflow.isFumble) {
--- macros/criticalHitsChangeDamageRoll.js
+++ macros/criticalHitsChangeDamageRoll.js
@@ -1,3 +1,6 @@
+// In theory, we could use this to change the damage roll for a critical hit. Unfortunately it is very difficult apply
+// the damage rolls that are given in the critical-hits-revisited.pdf accordingly. The damage rolls are given as strings
+// and are not easily converted to Roll objects. Therefore, we will have to use a different approach in the future.
 if (args[0].macroPass == 'preDamageRollComplete' && workflow.isCritical) {
     console.log('---------------------------------------------------------------');
     console.log(workflow);
