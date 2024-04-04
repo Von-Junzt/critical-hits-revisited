ae33a544665e5fd3de0876641115488158c8545b
aushilfsalien <nils.prior@outlook.de>
2024-04-04 10:39:44 +0200

    refactored the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,109 +1,68 @@
-async function rollForCriticalHits(workflowObject) {
-    // get last attack damage type
-    let lastAttack = workflowObject.damageItem;
-    let attackDamageType;
-
-    console.log(workflowObject);
-
-    // set the damageType to the one that delivered the highest damage amount
-    if(workflowObject.damageDetail.length > 0) {
-        // push keys and values into sorting array
-        let sortArray = [];
-        for (let i = 0; i < workflowObject.damageDetail.length; i++) {
-            sortArray.push([workflowObject.damageDetail[i].type, workflowObject.damageDetail[i].damage]);
+// Description: This file contains helper functions that are used in the main scripts.
+// applyEffects - This function applies one or multiple effects to the target.
+async function applyEffects(effects, target) {
+    if (typeof effects === 'string') {
+        await applyEffect(effects, target);
+    } else {
+        for (let effect of effects) {
+            await applyEffect(effect, target);
         }
+    }
+}
 
-        // sort array descending by damage
-        sortArray = sortArray.sort(([,a],[,b]) => b-a);
-        let maxDamageValue = sortArray[0][1];
-
-        // filter for highest damage
-        sortArray = sortArray.filter(element => (element[1] === maxDamageValue))
-
-        // if multiple damage types with same amount of damage, get the base damageType. at this point we assume, that
-        // weapons always have a single base damageType.
-        // in the future: if multiple damage types with same amount of damage, let user decide the table to roll on
-        if(sortArray.length > 1) {
-            for (let i = 0; i < workflowObject.damageDetail.length; i++) {
-                if(['bludgeoning', 'slashing', 'piercing'].includes(workflowObject.damageDetail[i].type)) {
-                    attackDamageType = workflowObject.damageDetail[i].type;
-                }
+// rollForCriticalHits - This function gets the damageType of the attack and rolls for critical hits based on the damageType.
+async function rollForCriticalHits(workflowObject) {
+    let attackDamageType = getAttackDamageType(workflowObject);
+    if (!["none", "no type", "no damage", "temphp", ""].includes(attackDamageType)) {
+        let tableName = attackDamageType.charAt(0).toUpperCase() + attackDamageType.slice(1);
+        let target = workflowObject.damageItem.actorUuid;
+        let rollTablePack = game.packs.get("critical-hits-revisited.critical-hits-tables");
+        rollTablePack.getIndex();
+        let rollTableID = rollTablePack.index.find(t => t.name === tableName)._id;
+        let rollResult = await rollTablePack.getDocument(rollTableID).then(table => table.draw({rollMode: "gmroll"}));
+        for (let result of rollResult.results) {
+            let rollRange = result.range.toString();
+            tableName = result.parent.name === "Minor Injuries" ? 'MinorInjuries' : result.parent.name === "Major Injuries" ? 'MajorInjuries' : tableName;
+            let appliedEffect = linkedEffects[tableName][rollRange];
+            if (appliedEffect) {
+                await applyEffects(appliedEffect, target);
             }
+        }
+    }
+}
+
+// rollForCriticalFumbles - This function rolls on the Critical Fumbles table and applies the effect to the target.
+async function rollForCriticalFumbles(workflowObject){
+    let attackDamageType = workflowObject.item.labels.damageType;
+    if (!["none", "no type", "no damage", "temphp", ""].includes(attackDamageType)) {
+        let target = workflowObject.tokenUuid;
+        let rollTablePack = game.packs.get("critical-hits-revisited.critical-hits-tables");
+        rollTablePack.getIndex();
+        let rollTableID = "TIJizkcNCKbq2qWD";
+        let rollResult = await rollTablePack.getDocument(rollTableID).then(table => table.draw({rollMode: "gmroll"}));
+        let rollRange = rollResult.results[0].range.toString();
+        let appliedEffect = linkedEffects['Fumble'][rollRange];
+        if (appliedEffect) {
+            await applyEffects(appliedEffect, target);
+        }
+    }
+}
+
+// getAttackDamageType - This function gets the attack damage type from the workflowObject.
+function getAttackDamageType(workflowObject) {
+    let attackDamageType;
+    if(workflowObject.damageDetail.length > 0) {
+        let sortArray = workflowObject.damageDetail.map(detail => [detail.type, detail.damage]);
+        sortArray.sort(([,a],[,b]) => b-a);
+        let maxDamageValue = sortArray[0][1];
+        sortArray = sortArray.filter(element => (element[1] === maxDamageValue))
+        if(sortArray.length > 1) {
+            attackDamageType = workflowObject.damageDetail.find(detail => ['bludgeoning', 'slashing', 'piercing'].includes(detail.type)).type;
         } else {
             attackDamageType = sortArray[0][0];
         }
     } else {
         attackDamageType = workflowObject.damageDetail[0].type;
     }
-    // exclude certain damage types because there are no rolltables
-    if (!(["none", "no type", "no damage", "temphp", ""].includes(attackDamageType))) {
-        // get damageType and customize
-        let tableName = attackDamageType.charAt(0).toUpperCase() + attackDamageType.slice(1);
-        // get rolltable
-        let rollTablePack = game.packs.get("critical-hits-revisited.critical-hits-tables");
-        // get target
-        let target = workflowObject.damageItem.actorUuid;
-
-        // prepare rolltable
-        rollTablePack.getIndex();
-        let rollTableID = rollTablePack.index.find(t => t.name === tableName)._id;
-
-        // roll on table and apply effect
-        let rollResult = await rollTablePack.getDocument(rollTableID).then(table => table.draw({rollMode: "gmroll"}));
-
-        for (let i = 0; i < rollResult.results.length; i++) {
-            let rollRange = rollResult.results[i].range.toString();
-            if (rollResult.results[i].parent.name === "Minor Injuries") {
-                tableName = 'MinorInjuries';
-            } else if (rollResult.results[i].parent.name === "Major Injuries") {
-                tableName = 'MajorInjuries';
-            }
-
-            if (!(linkedEffects[tableName][rollRange] === undefined)) {
-                let appliedEffect = linkedEffects[tableName][rollRange];
-                // apply Effect
-                if (typeof appliedEffect === 'string') {
-                    applyEffect(appliedEffect, target);
-                } else {
-                    for (let i = 0; i < appliedEffect.length; i++) {
-                        applyEffect(appliedEffect[i], target);
-                    }
-                }
-            }
-        }
-    }
-}
-
-async function rollForCriticalFumbles(workflowObject){
-
-    // get last attack damage type
-    let attackDamgeType = workflowObject.item.labels.damageType;
-
-    // exclude certain damage types because there are no rolltables
-    if (!(["none", "no type", "no damage", "temphp", ""].includes(attackDamgeType))) {
-
-        // get target
-        let target = workflowObject.tokenUuid;
-
-        // get rolltable
-        let rollTablePack = game.packs.get("critical-hits-revisited.critical-hits-tables");
-        rollTablePack.getIndex();
-        let rollTableID = "TIJizkcNCKbq2qWD";
-
-        // roll on table
-        let rollResult = await rollTablePack.getDocument(rollTableID).then(table => table.draw({rollMode: "gmroll"}));
-        let rollRange = rollResult.results[0].range.toString();
-
-        if (!(linkedEffects['Fumble'][rollRange] === undefined)) {
-            let appliedEffect = linkedEffects['Fumble'][rollRange];
-            // apply Effect
-            if (typeof appliedEffect === 'string') {
-                applyEffect(appliedEffect, target);
-            } else {
-                for (let i = 0; i < appliedEffect.length; i++) {
-                    applyEffect(appliedEffect[i], target);
-                }
-            }
-        }
-    }
+    return attackDamageType;
 }
