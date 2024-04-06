3756a389db0ab9a60365332b884fd2c887a27b69
aushilfsalien <nils.prior@outlook.de>
2024-04-06 16:38:39 +0200

    restored return in checkImmunity

--- scripts/helperFunctions.js
+++ scripts/helperFunctions.js
@@ -43,7 +43,7 @@ export const helperFunctions = {
         // This ChatMessage is for debug only
         // this.capitalizeFirstLetter(effect);
         // await helperFunctions.createChatMessage(targetUuid, `<div class="result-text"><b>${effect}</b> - ` + target.name + ` is not immune.</div>`);
-        // return false;
+        return false;
     },
     // createChatMessage - This function creates a chat message with the given speaker and message content.
     createChatMessage: async function (speaker, msgContent) {
