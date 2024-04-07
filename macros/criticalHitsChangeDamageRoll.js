4b1d903ff8cba85c6174c2e8b908e5c16acdbf29
aushilfsalien <nils.prior@outlook.de>
2024-04-07 12:49:47 +0200

    added comments

--- macros/criticalHitsChangeDamageRoll.js
+++ macros/criticalHitsChangeDamageRoll.js
@@ -1,10 +1,8 @@
-// In theory, we could use this to change the damage roll for a critical hit. Unfortunately it is very difficult apply
-// the damage rolls that are given in the critical-hits-revisited.pdf accordingly. The damage rolls are given as strings
-// and are not easily converted to Roll objects. Therefore, we will have to use a different approach in the future.
+// In theory, we could use this to change the damage roll for a critical hit. Technically it's not possible, because we choose
+// the damage type depending on the highest damage value after immunities and resistances are applied. For my dnd5e group
+// this is the most realistic approach.
 if (args[0].macroPass == 'preDamageRollComplete' && workflow.isCritical) {
-    console.log('---------------------------------------------------------------');
     console.log(workflow);
-    let weaponDamage = workflow.item.
     let damageRoll = await new Roll("7").roll({async: true});
     damageRoll = await workflow.convertRollToDamageRoll(damageRoll);
     workflow.setDamageRoll(damageRoll);
