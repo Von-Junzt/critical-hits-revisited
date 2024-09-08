0b2f167da634f3ae0895f84ff8c9a04bfaadd495
aushilfsalien <nils.prior@outlook.de>
2024-09-08 14:18:09 +0200

    refactored file structure

---
+++ effectmacros/decapitated.js
@@ -0,0 +1,7 @@
+import { acidBath } from '../effectMacros/acidBath.js';
+import { decapitated } from '../effectMacros/decapitated.js';
+
+export const effectMacros = {
+    acidBath,
+    decapitated
+};
--- scripts/effectMacros.js
+++ scripts/effectMacros.js
@@ -17,14 +17,14 @@ export const effectMacros = {
             await helperFunctions.createChatMessage(target, '<div class="result-text"><b>Acid Bath</b> - The acid of the last attack has rendered ' + target.name + 's armor useless! It has magical properties so it can be repaired.' + '</div>');
         } else {
             equippedArmor.delete();
-            await helperFunctions.createChatMessage(target, '<div class="result-text"><b>Acid Bath</b> - The acid of the last attack has destroyed ' + target.name + 's armor because it has no magical properties!' + '</div>');
+            await helperFunctions.createChatMessage(target, '<div class="result-text"><b>Acid Bath</b> - The acid of the last attack has destroyed ' + target.name + 's armor because it had no magical properties!' + '</div>');
         }
     },
     decapitated: async function (target) {
         // TODO: If the target is a player, the GM should be asked if the player should die or not.
         // check if the target is a player or a creature
         if (target.type === 'character') {
-            ui.notifications.warn('The target is a player. The player should be asked if they want to die or not.');
+            ui.notifications.warn('The target is a player. The GM should be asked if they want the player to die or not.');
             target.update({"system.attributes.hp.value" : 0})
         } else {
             // set the hitpoints to 0
