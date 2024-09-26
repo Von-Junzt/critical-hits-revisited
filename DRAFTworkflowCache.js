df598c56697cf06ffd6a82a632f2ba11cad01749
Von-Junzt <nils.prior@outlook.de>
2024-09-26 10:28:35 +0200

    Create DRAFTworkflowCache.js
    Drafted a reworke workflowCache.js

---
+++ DRAFTworkflowCache.js
@@ -0,0 +1,48 @@
+import { mainScriptUtils } from "./mainScriptUtils.js";
+
+export const workflowCache = {
+    WORKFLOW_ITEM_NAME: 'CriticalHitsRevisitedWorkflow',
+
+    async getWorkflowItem() {
+        let workflowItem = game.items.getName(this.WORKFLOW_ITEM_NAME);
+        if (!workflowItem) {
+            workflowItem = await Item.create({
+                name: this.WORKFLOW_ITEM_NAME,
+                type: 'data', // Adjust this type as needed
+                data: null
+            });
+        }
+        return workflowItem;
+    },
+
+    async saveWorkflow(workflow) {
+        const workflowItem = await this.getWorkflowItem();
+        await workflowItem.update({ data: workflow });
+        mainScriptUtils.debug('Workflow saved.');
+    },
+
+    async deleteWorkflow() {
+        const workflowItem = await this.getWorkflowItem();
+        await workflowItem.update({ data: null });
+        mainScriptUtils.debug('Workflow deleted.');
+    },
+
+    async getWorkflow() {
+        const workflowItem = await this.getWorkflowItem();
+        return workflowItem.data;
+    },
+
+    async getWorkflowByTargetUUID(uuid) {
+        const workflow = await this.getWorkflow();
+        if (!workflow) return null;
+
+        if (Array.isArray(workflow.damageList)) {
+            if (workflow.damageList.some(actor => actor.actorUuid === uuid)) {
+                return workflow;
+            }
+        } else if (workflow.damageList.actorUuid === uuid) {
+            return workflow;
+        }
+        return null;
+    }
+};
