7ff6c065178a0a4022ba9d3c4759fdb7f79fd0bc
aushilfsalien <nils.prior@outlook.de>
2024-09-27 22:45:47 +0200

    implemented sentReeling effect. works but still doubles dialog for gm

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ Hooks.once('ready', () => {
 
 Hooks.on('midi-qol.preItemRoll', async (workflow) => {
     await workflowCache.deleteWorkflow();
+    await workflowCache.deleteCachedDialogResult();
     await critCheckWorkflow.checkForCritsOnOther(workflow);
 });
 
