3c8a22258b944c1cd08625be3db93a6b60cf14ec
aushilfsalien <nils.prior@outlook.de>
2024-10-02 19:21:30 +0200

    added acidBath animation, made animations optional

--- settings.js
+++ settings.js
@@ -1,6 +1,7 @@
 import { OPTIONS, updateOptions } from "./options.js";
 
 export const registerSettings = function() {
+  // Should critical hits be applied to spells without an action type?
   game.settings.register("critical-hits-revisited", "critsOnOtherEnabled", {
     name: "Enable Crits on Other Spells",
     hint: "If enabled, spells with non-standard action types can crit.",
@@ -12,7 +13,7 @@ export const registerSettings = function() {
       OPTIONS.CRITS_ON_OTHER_ENABLED = value;
     }
   });
-
+  // Enable debug logging
   game.settings.register("critical-hits-revisited", "enableDebug", {
     name: "Enable Debug Logging",
     hint: "Turn on detailed debug logging for Critical Hits Revisited",
@@ -22,6 +23,18 @@ export const registerSettings = function() {
     default: false,
     onChange: value => {
       OPTIONS.DEBUG_MODE = value;
+    }
+  });
+  // Enable custom effect animations
+  game.settings.register("critical-hits-revisited", "enableAnimations", {
+    name: "Enable Animations",
+    hint: "If enabled, animations will play for critical hits.",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    onChange: value => {
+      OPTIONS.ANIMATIONS_ENABLED = value;
     }
   });
 
