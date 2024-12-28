121d5726f7ddad7a1ba6f4da8a24b25ecc81cd1f
aushilfsalien <nils.prior@outlook.de>
2024-12-28 23:15:15 +0100

    removed setting that was not needed anymore

--- settings.js
+++ settings.js
@@ -1,18 +1,6 @@
 import { OPTIONS, updateOptions } from "./options.js";
 
 export const registerSettings = function() {
-  // Should critical hits be applied to spells without an action type?
-  game.settings.register("critical-hits-revisited", "critsOnOtherEnabled", {
-    name: "Enable Crits on Other Spells",
-    hint: "If enabled, spells with non-standard action types can crit.",
-    scope: "world",
-    config: true,
-    type: Boolean,
-    default: true,
-    onChange: value => {
-      OPTIONS.CRITS_ON_OTHER_ENABLED = value;
-    }
-  });
   // Enable debug logging
   game.settings.register("critical-hits-revisited", "enableDebug", {
     name: "Enable Debug Logging",
